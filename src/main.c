5775d556e8667f6be8f802328c9034f62b5790a5
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-05 22:58:46 +0200

    try miniLibX

---
+++ src/main.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 20:35:01 by aestrell          #+#    #+#             */
+/*   Updated: 2024/05/05 13:30:29 by aestrell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mlx/mlx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+
+typedef struct s_data
+{
+	void *mlx_ptr;
+	void *win_ptr;
+} t_data;
+ 
+int on_destroy(t_data *data)
+{
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	exit(0);
+	return (0);
+}
+ 
+int on_keypress(int keysym, t_data *data)
+{
+	(void)data;
+	printf("Pressed key: %d\\n", keysym);
+	return (0);
+}
+ 
+int main(void)
+{
+	t_data data;
+ 
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "hi :)");
+	if (!data.win_ptr)
+		return (free(data.mlx_ptr), 1);
+ 
+	// Register key release hook
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
+ 
+	// Register destroy hook
+	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+ 
+	// Loop over the MLX pointer
+	mlx_loop(data.mlx_ptr);
+	return (0);
+}
