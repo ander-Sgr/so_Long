86018ef2ab6f8de27142e79cd27e08346b206c24
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-12 21:00:42 +0200

    wip draw xpm in windows

--- src/read_map.c
+++ src/read_map.c
@@ -6,21 +6,20 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 01:01:06 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/11 19:33:33 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/12 16:56:48 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_get_map_dimensions(char *file_map, t_map *map)
+static void	ft_get_map_dimensions(char *file_map, t_map *map)
 {
 	int		fd;
-	int		max_width;
 	int		height;
 	char	*line;
 	int		width;
 
-	max_width = 0;
+	width = 0;
 	height = 0;
 	fd = open(file_map, O_RDONLY);
 	if (fd < 0)
@@ -31,17 +30,15 @@ void	ft_get_map_dimensions(char *file_map, t_map *map)
 		if (line == NULL)
 			break ;
 		width = strlen(line);
-		if (width > max_width)
-			max_width = width;
 		height++;
 		free(line);
 	}
 	close(fd);
-	map->width = max_width;
+	map->width = width;
 	map->height = height;
 }
 
-char	**ft_read_map(char *file_map, t_map *map)
+static char	**ft_read_map(char *file_map, t_map *map)
 {
 	int		fd;
 	char	*line;
@@ -67,4 +64,10 @@ char	**ft_read_map(char *file_map, t_map *map)
 	}
 	close(fd);
 	return (map->map);
+}
+
+void	init_map(char *file_map, t_map *map)
+{
+	ft_get_map_dimensions(file_map, map);
+	map->map = ft_read_map(file_map, map);
 }
--- src/so_long.h
+++ src/so_long.h
@@ -6,20 +6,31 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 22:53:10 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/11 19:10:24 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/12 20:55:03 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-
 # include "../gnl/get_next_line.h"
 # include "../libft/libft.h"
+# include "../mlx/mlx.h"
 # include <X11/X.h>
 # include <X11/keysym.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# ifndef WIN_HEIGHT
+#  define WIN_HEIGHT 500
+# endif
+
+# ifndef WIN_WIDTH
+#  define WIN_WIDTH 900
+# endif
+
+# ifndef TILE_SIZE
+#  define TILE_SIZE 80
+# endif
 
 // map stuct
 typedef struct s_pos
@@ -39,6 +50,7 @@ typedef struct s_mlx
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+
 }			t_mlx;
 
 typedef struct s_square
@@ -49,7 +61,8 @@ typedef struct s_square
 
 }			t_square;
 
-char		**ft_read_map(char *file_map, t_map *map);
-void		ft_get_map_dimensions(char *file_map, t_map *map);
+void		init_map(char *file_map, t_map *map);
+void		draw_xpm(t_mlx *mlx, char *file_name, int pos_x, int pos_y);
+void		draw_map(t_mlx *mlx, t_map *map);
 
 #endif
