84d1ff86e193d10b4d5c6c96765f892bc0973f37
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-12 21:02:08 +0200

    wip: restructure code

--- src/main.c
+++ src/main.c
@@ -6,41 +6,29 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 20:35:01 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/11 19:36:17 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/12 19:58:09 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../mlx/mlx.h"
 #include "so_long.h"
 
 int	main(void)
 {
-    t_map map;
-    char *file_name = "./maps/map1.ber";
+	t_mlx	mlx;
+	t_map	map;
 
-    ft_get_map_dimensions(file_name, &map);
-    map.map = ft_read_map(file_name, &map);
-
-    if (map.map == NULL) {
-        printf("Error al leer el mapa\n");
-        return 1;
-    }
-
-    printf("Ancho del mapa: %d\n", map.width);
-    printf("Altura del mapa: %d\n", map.height);
-    printf("Contenido del mapa:\n");
-    for (int y = 0; y < map.height; y++) {
-        for (int x = 0; x < map.width; x++) {
-            printf("%c ", map.map[y][x]);
-        }
-        printf("\n");
-    }
-
-    // Liberar memoria utilizada para almacenar el mapa
-    for (int i = 0; i < map.height; i++) {
-        free(map.map[i]);
-    }
-    free(map.map);
-
-    return 0;
+	mlx.mlx_ptr = mlx_init();
+	if (mlx.mlx_ptr == NULL)
+		return (1);
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, WIN_HEIGHT, WIN_HEIGHT,
+			"so_long");
+	if (mlx.win_ptr == NULL)
+		return (1);
+	init_map("./maps/map1.ber", &map);
+	if (map.map == NULL)
+		printf("Error read map: \n");
+	printf("width map: %d\nheight map: %d\n", map.width, map.height);
+	draw_map(&mlx, &map);
+	mlx_loop(mlx.mlx_ptr);
+	return (0);
 }
