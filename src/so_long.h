33e290cc98b6852a706e426c4861a5f707c97383
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-16 22:55:57 +0200

    wip: restruct data

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 22:53:10 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/12 20:55:03 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/16 22:48:12 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 # ifndef WIN_HEIGHT
-#  define WIN_HEIGHT 500
+#  define WIN_HEIGHT 400
 # endif
 
 # ifndef WIN_WIDTH
-#  define WIN_WIDTH 900
+#  define WIN_WIDTH 600
 # endif
 
 # ifndef TILE_SIZE
-#  define TILE_SIZE 80
+#  define TILE_SIZE 64
 # endif
 
 // map stuct
@@ -42,27 +42,49 @@ typedef struct s_pos
 typedef struct s_map
 {
 	char	**map;
-	int width;  // height
-	int height; // width
-}			t_map;
+	int		width;
+	int		height;
+}	t_map;
 
 typedef struct s_mlx
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-
+	int		width_win;
+	int		height_win;
 }			t_mlx;
 
-typedef struct s_square
+typedef struct s_player
 {
-	t_pos	pos;
-	int		size;
-	int		color;
+	t_pos	pos_player;
+	int		items_recollected;
+	int		mov_count;
+}			t_player;
 
-}			t_square;
+typedef	struct s_img
+{
+	t_mlx	mlx;
+	t_pos	pos_img;
+	int		tile_size;
+	char	*img_path;
+	char	*addr;
+	int		pixels;
+	int		endian;
+}			t_img;
 
-void		init_map(char *file_map, t_map *map);
-void		draw_xpm(t_mlx *mlx, char *file_name, int pos_x, int pos_y);
-void		draw_map(t_mlx *mlx, t_map *map);
+
+typedef struct s_game
+{
+	t_mlx		mlx;
+	t_player	player;
+	t_map		map;
+	t_img		wall;
+	t_img		floor;
+	t_img		character;
+}			t_game;
+
+void	init_map(char *file_map, t_map *map);
+void	draw_xpm(t_mlx *mlx, char *file_name, int pos_x, int pos_y);
+void	draw_map(t_mlx *mlx, t_map *map);
 
 #endif
