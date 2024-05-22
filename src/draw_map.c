4c6837b8dff2c290ce6727e2fb91f2fd57459056
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-22 02:50:40 +0200

    wip

--- src/draw_map.c
+++ src/draw_map.c
@@ -6,14 +6,11 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 17:46:42 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/20 19:46:18 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/22 02:13:44 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-// x - width
-// y - height
 
 void	ft_check_elements_map(t_game *game, int x, int y)
 {
@@ -27,6 +24,12 @@ void	ft_check_elements_map(t_game *game, int x, int y)
 	else if (game->map.map[x][y] == 'P')
 		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win_ptr,
 				game->character.img_ptr, y * TILE_SIZE, x * TILE_SIZE);
+	else if (game->map.map[x][y] == 'C')
+		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win_ptr,
+				game->item.img_ptr, y * TILE_SIZE, x * TILE_SIZE);
+	else if (game->map.map[x][y] == 'E')
+		mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.win_ptr,
+				game->exit.img_ptr, y * TILE_SIZE, x * TILE_SIZE);
 }
 
 void	ft_draw_map(t_game *game)
