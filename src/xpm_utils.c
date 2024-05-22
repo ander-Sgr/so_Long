0a2f6546aaea58032ed6abeb20c177db6847d436
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-22 02:49:32 +0200

    refactor xpm utils & xpm files

--- src/xpm_utils.c
+++ src/xpm_utils.c
@@ -6,56 +6,71 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 17:06:00 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/20 19:55:09 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/22 02:48:21 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static int	ft_load_xpm(t_game *game, t_img *img, char *file_path)
+static t_img	**ft_storage_images(t_game *game, int img_count)
 {
-	img->img_ptr = mlx_xpm_file_to_image(game->mlx.mlx_ptr, file_path,
-			&img->width, &img->heigth);
-	if (!img->img_ptr)
-	{
-		fprintf(stderr, "Error loading XPM file: %s\n", file_path);
-		return (0);
-	}
-	img->addr = mlx_get_data_addr(img->img_ptr, &img->bits_per_pixel,
-			&img->size_line, &img->endian);
-	return (1);
+	t_img	**images;
+
+	images = (t_img **)malloc(img_count * sizeof(t_img *));
+	if (!images)
+		return (NULL);
+	images[0] = &game->wall;
+	images[1] = &game->floor;
+	images[2] = &game->character;
+	images[3] = &game->item;
+	images[4] = &game->exit;
+	return (images);
 }
 
-/* 
-static int	ft_check_errors_xpm(t_game *game)
+static void	ft_clean_xpm(t_game *game, t_img **images, int img_count)
+{
+	int	i;
+
+	i = 0;
+	while (i < img_count)
+	{
+		if (images[i]->img_ptr)
+		{
+			mlx_destroy_image(game->mlx.mlx_ptr, images[i]->img_ptr);
+			images[i]->img_ptr = NULL;
+		}
+		i++;
+	}
+}
+
+int	ft_check_errors_xpm(t_game *game)
 {
 	int		img_count;
 	int		err_count;
 	int		i;
-	t_img	*images[2];
+	t_img	**images;
 
-	img_count = 2;
+	img_count = 5;
 	err_count = 0;
-	images[0] = &game->wall;
-	images[1] = &game->floor;
-	i = 0;
-	while (i < img_count)
+	images = ft_storage_images(game, img_count);
+	if (!images)
+	{
+		return (0, free(images));
+	}
+	for (i = 0; i < img_count; i++)
 	{
 		if (!images[i]->img_ptr)
 			err_count++;
-		i++;
 	}
 	if (err_count > 0)
 	{
-		i = 0;
-		while (i < img_count)
+		for (i = 0; i < img_count; i++)
 		{
 			if (images[i]->img_ptr)
 			{
 				mlx_destroy_image(game->mlx.mlx_ptr, images[i]->img_ptr);
 				images[i]->img_ptr = NULL;
 			}
-			i++;
 		}
 		if (game->mlx.win_ptr)
 		{
@@ -67,43 +82,36 @@ static int	ft_check_errors_xpm(t_game *game)
 			free(game->mlx.mlx_ptr);
 			game->mlx.mlx_ptr = NULL;
 		}
+		printf("Could not load all xpm files\n");
+		free(images);
 		return (0);
 	}
+	free(images);
 	return (1);
-} */
+}
+static int	ft_load_xpm(t_game *game, t_img *img, char *file_path)
+{
+	img->img_ptr = mlx_xpm_file_to_image(game->mlx.mlx_ptr, file_path,
+			&img->width, &img->heigth);
+	if (!img->img_ptr)
+	{
+		ft_check_errors_xpm(game);
+		return (0);
+	}
+	img->addr = mlx_get_data_addr(img->img_ptr, &img->bits_per_pixel,
+			&img->size_line, &img->endian);
+	img->tile_size = TILE_SIZE;
+	img->img_path = file_path;
+	return (1);
+}
 t_game	*ft_init_images(t_game *game)
 {
 	if (!ft_load_xpm(game, &game->wall, "./textures/wall.xpm") ||
 		!ft_load_xpm(game, &game->floor, "./textures/floor.xpm") ||
-		!ft_load_xpm(game, &game->character, "./textures/character.xpm"))
-		return (NULL);
+		!ft_load_xpm(game, &game->character, "./textures/character.xpm") ||
+		!ft_load_xpm(game, &game->item, "./textures/item.xpm") ||
+		!ft_load_xpm(game, &game->exit, "./textures/exit.xpm"))
+		return (0);
+
 	return (game);
 }
-
-/*
-void	*ft_load_xpm(t_mlx *mlx, char *file_name, int *width, int *height) {
-    void *img;
-
-    img = mlx_xpm_file_to_image(mlx->mlx_ptr, file_name, width, height);
-    if (img == NULL) {
-        printf("Error al cargar la imagen XPM\n");
-        return (NULL);
-    }
-    return (img);
-}
-void	draw_xpm(t_mlx *mlx, char *file_name, int pos_x, int pos_y)
-{
-	int		width;
-	int		height;
-	void	*img_ptr;
-
-	img_ptr = ft_load_xpm(mlx, file_name, &width, &height);
-	if (!img_ptr)
-	{
-		printf("Error al cargar la imagen XPM\n");
-		return ;
-	}
-	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, img_ptr, pos_x
-			* TILE_SIZE, pos_y * TILE_SIZE);
-}
-*/
