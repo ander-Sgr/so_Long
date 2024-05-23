72b627fdf78ba8adb5b2f411058e6187ae2a8634
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-24 01:19:09 +0200

    wip

--- src/main.c
+++ src/main.c
@@ -6,33 +6,68 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 20:35:01 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/14 17:06:18 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/24 00:50:41 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	initialize_game(t_game *game)
+{
+	// Inicialización de las estructuras básicas
+	game->mlx.mlx_ptr = NULL;
+	game->mlx.win_ptr = NULL;
+	game->player.pos_player.x = 0;
+	game->player.pos_player.y = 0;
+	game->player.items_recollected = 0;
+	game->player.mov_count = 0;
+	game->map.map = NULL;
+	game->map.width = 0;
+	game->map.height = 0;
+	// Inicialización de las imágenes (sin cargar aún)
+	game->wall.img_ptr = NULL;
+	game->wall.addr = NULL;
+	game->floor.img_ptr = NULL;
+	game->floor.addr = NULL;
+	game->character.img_ptr = NULL;
+	game->character.addr = NULL;
+	game->item.img_ptr = NULL;
+	game->item.addr = NULL;
+	game->exit.img_ptr = NULL;
+	game->exit.addr = NULL;
+}
+
 int	main(void)
 {
-	t_mlx	mlx;
-	t_map	map;
+	t_game	game;
 	int		tile_width;
 	int		tile_height;
 
-	mlx.mlx_ptr = mlx_init();
-	if (mlx.mlx_ptr == NULL)
+	// Inicializar el juego
+	initialize_game(&game);
+	// Inicializar MLX
+	game.mlx.mlx_ptr = mlx_init();
+	// Inicializar el mapa
+	ft_init_map("./maps/map1.er", &game);
+	if (!game.map.map)
+	{
 		return (1);
-	init_map("./maps/map1.ber", &map);
-	if (map.map == NULL)
-		printf("Error read map: \n");
-	tile_width =  map.width * TILE_SIZE;
-	tile_height = map.height * TILE_SIZE;
-	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, tile_width, tile_height,
+	}
+	// Crear una ventana
+	tile_width = game.map.width * TILE_SIZE;
+	tile_height = game.map.height * TILE_SIZE;
+	game.mlx.win_ptr = mlx_new_window(game.mlx.mlx_ptr, tile_width, tile_height,
 			"so_long");
-	if (mlx.win_ptr == NULL)
+	if (game.mlx.win_ptr == NULL)
+	{
+		free(game.mlx.mlx_ptr);
 		return (1);
-	printf("width map: %d\nheight map: %d\n", map.width, map.height);
-	draw_map(&mlx, &map);
-	mlx_loop(mlx.mlx_ptr);
+	}
+	// Inicializar imágenes
+	if (ft_init_images(&game))
+	{
+		ft_draw_map(&game);
+		mlx_loop(game.mlx.mlx_ptr);
+	}
 	return (0);
 }
--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 22:53:10 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/16 22:48:12 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/24 00:51:51 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,46 +32,45 @@
 #  define TILE_SIZE 64
 # endif
 
-// map stuct
 typedef struct s_pos
 {
-	int		x;
-	int		y;
-}			t_pos;
+	int			x;
+	int			y;
+}				t_pos;
 
 typedef struct s_map
 {
-	char	**map;
-	int		width;
-	int		height;
-}	t_map;
+	char		**map;
+	int			width;
+	int			height;
+}				t_map;
 
 typedef struct s_mlx
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	int		width_win;
-	int		height_win;
-}			t_mlx;
+	void		*mlx_ptr;
+	void		*win_ptr;
+}				t_mlx;
 
 typedef struct s_player
 {
-	t_pos	pos_player;
-	int		items_recollected;
-	int		mov_count;
-}			t_player;
+	t_pos		pos_player;
+	int			items_recollected;
+	int			mov_count;
+}				t_player;
 
-typedef	struct s_img
+typedef struct s_img
 {
-	t_mlx	mlx;
-	t_pos	pos_img;
-	int		tile_size;
-	char	*img_path;
-	char	*addr;
-	int		pixels;
-	int		endian;
-}			t_img;
-
+	void		*img_ptr;
+	char		*img_path;
+	char		*addr;
+	t_pos		pos_img;
+	int			width;
+	int			heigth;
+	int			tile_size;
+	int			bits_per_pixel;
+	int			size_line;
+	int			endian;
+}				t_img;
 
 typedef struct s_game
 {
@@ -81,10 +80,12 @@ typedef struct s_game
 	t_img		wall;
 	t_img		floor;
 	t_img		character;
-}			t_game;
+	t_img		item;
+	t_img		exit;
+}				t_game;
 
-void	init_map(char *file_map, t_map *map);
-void	draw_xpm(t_mlx *mlx, char *file_name, int pos_x, int pos_y);
-void	draw_map(t_mlx *mlx, t_map *map);
+void			ft_init_map(char *file_map, t_game *game);
+void			ft_draw_map(t_game *game);
+t_game			*ft_init_images(t_game *game);
 
 #endif
