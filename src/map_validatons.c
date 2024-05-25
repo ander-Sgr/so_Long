3ce7842cd82acaa253e011f8b397408639ae86fa
ander-Sgr <andersonbarreto2001@gmail.com>
2024-05-26 01:11:22 +0200

    feat: add validations map

--- src/map_validatons.c
+++ src/map_validatons.c
@@ -6,13 +6,25 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 23:07:06 by aestrell          #+#    #+#             */
-/*   Updated: 2024/05/24 01:13:31 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/05/26 01:10:39 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../so_long.h"
+#include "so_long.h"
 
 int	*ft_check_file(char *file_map)
 {
+    /**
+     * podrimoas extreaer los ultmos 3 caracteres de la ruta del mapa 
+     * verificamos que coincida con la ext .bet si coincide devolvemos true si no
+     * false
+     */
     
+    printf("file path: ", file_map);
+    
+    
+
+
 }
+
+// to do verifiar la estructura del mapa por ej 1ra  linea es vacia o no 
