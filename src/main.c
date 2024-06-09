36389d7ffda34b13d7d60e644bce9b3de22c86b3
ander-Sgr <andersonbarreto2001@gmail.com>
2024-06-09 20:55:04 +0200

    make some refactor

--- src/main.c
+++ src/main.c
@@ -6,66 +6,24 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 20:35:01 by aestrell          #+#    #+#             */
-/*   Updated: 2024/06/06 22:02:04 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/06/09 20:38:55 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	init_pos(t_pos *pos)
-{
-	pos->x = 0;
-	pos->y = 0;
-}
-
-void	initialize_game(t_game *game)
-{
-	// Inicialización de las estructuras básicas
-	game->mlx.mlx_ptr = NULL;
-	game->mlx.win_ptr = NULL;
-	game->player.pos_player.x = 0;
-	game->player.pos_player.y = 0;
-	game->player.items_recollected = 0;
-	game->player.mov_count = 0;
-	game->map.map = NULL;
-	game->map.width = 0;
-	game->map.height = 0;
-	game->map.character_count = 0;
-	game->map.exit_count = 0;
-	game->map.item_count = 0;
-	game->map.found_exit = 0;
-	game->map.collected_item = 0;
-	// Inicialización de las imágenes (sin cargar aún)
-	game->wall.img_ptr = NULL;
-	game->wall.addr = NULL;
-	game->floor.img_ptr = NULL;
-	game->floor.addr = NULL;
-	game->character.img_ptr = NULL;
-	game->character.addr = NULL;
-	game->item.img_ptr = NULL;
-	game->item.addr = NULL;
-	game->exit.img_ptr = NULL;
-	game->exit.addr = NULL;
-}
 int	main(void)
 {
 	t_game	game;
-	t_pos	pos;
 	int		tile_width;
 	int		tile_height;
 	char	*file_map;
 
 	file_map = "./maps/map1.ber";
-	// Inicializar el juego
-	initialize_game(&game);
-	init_pos(&pos);
-	// Inicializar MLX
+	ft_init_vars(&game);
 	game.mlx.mlx_ptr = mlx_init();
 	if (game.mlx.mlx_ptr == NULL)
-	{
 		printf("Error initializing MLX\n");
-	}
-	// Inicializar el mapa
 	if (ft_init_map(file_map, &game))
 	{
 		tile_width = game.map.width * TILE_SIZE;
@@ -77,11 +35,7 @@ int	main(void)
 			printf("Error creating window\n");
 			free(game.mlx.mlx_ptr);
 		}
-		// Inicializar imágenes & mapa
 		if (ft_init_images(&game) && ft_draw_map(&game))
-		{
-			printf("\nwidth %d - heigth %d\n", game.map.width, game.map.height);
 			mlx_loop(game.mlx.mlx_ptr);
-		}
 	}
 }
--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: aestrell <aestrell@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 01:01:06 by aestrell          #+#    #+#             */
-/*   Updated: 2024/06/06 22:11:37 by aestrell         ###   ########.fr       */
+/*   Updated: 2024/06/09 20:39:53 by aestrell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,26 +86,10 @@ void	ft_cleanup_map(t_game *game)
 
 t_game	*ft_init_map(char *file_map, t_game *game)
 {
-	int		found_exit;
-	t_pos	start_pos;
-
-	game->map.map = NULL;
 	ft_get_map_dimensions(file_map, game);
 	game->map.map = ft_read_map(file_map, game);
-	ft_element_pos(game);
 	if (game->map.map == NULL || !ft_is_valid_map(game, file_map))
 	{
-		ft_cleanup_map(game);
-		return (NULL);
-	}
-	printf("Pos en init map x %d - y %d\n", game->player.pos_player.x,
-			game->player.pos_player.y);
-	start_pos = game->player.pos_player;
-	found_exit = flood_fill(game, &start_pos);
-	printf("%d", found_exit);
-	if (!found_exit)
-	{
-		printf("Error: no se pudo encontrar la salida en el mapa.\n");
 		ft_cleanup_map(game);
 		return (NULL);
 	}
